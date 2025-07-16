a6a101990e3f907857757e0f6ff9675d307d5dfc
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-07-16 10:42:38 +0300

    display a circle

--- src/Hooks/State/FormState.jsx
+++ src/Hooks/State/FormState.jsx
@@ -7,7 +7,7 @@ function FormState() {
         return Math.PI * radius * radius;
     }
     const updateRadiusValue = (e) => {
-        //console.log(e);
+        //console.log(e.target.value);
         setRadius(e.target.value);
     }
     return (
@@ -17,10 +17,36 @@ function FormState() {
                 <p><input onChange={updateRadiusValue} type="number" className='input-style'/></p>
             </div>
             <div>
-                For a cicle with radius <b>{radius}</b> Area is {" "}
+                For a circle with radius: <b>{radius}</b> Area is {" "}
                 <b>{areaOfCircle(radius)}</b>
             </div>
+            <Circle radius={radius}/>
         </div>
     );
+}
+
+function Circle(props) {
+    const { radius } = props;
+
+    const getSize = (radius) => {
+        let r = parseInt(radius);
+        return `${2 * r}px`;
+    }
+
+    return (
+        <div style={{ 
+                width: getSize(radius),
+                height: getSize(radius),
+                borderRadius: "2000px", 
+                backgroundColor: "grey",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+
+        }}>
+            <span>r={parseInt(radius)}</span>
+        </div>
+    );
+
 }
 export default FormState;
